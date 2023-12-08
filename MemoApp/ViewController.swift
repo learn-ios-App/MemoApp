476b073312f51e9be2f8ea06ddbaded8cdb4a397
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-12-08 18:51:32 +0900

    Initial Commit

---
+++ MemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoApp
+//
+//  Created by 渡邊魁優 on 2023/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
